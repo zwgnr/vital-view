d38fee6264f3ec190c8e18afed1b277892c836eb
zwagnr <0x@pop-os.localdomain>
2023-02-06 11:00:50 -0500

    improve auth flow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,27 @@
 import { type NextPage } from "next";
+import Router, { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import { SignInButton } from "../components/signInButton";
 import { TodaysScores } from "../components/todaysScores";
 import { Trends } from "../components/charts/overviewTrends";
 import { greeter } from "../lib/greeter";
-export const Home: NextPage = () => {
+import SignIn from "./sign-in";
+
+export const Home = () => {
   const { data: session, status } = useSession();
 
-  return (
-    <>
-      {status === "authenticated" ? (
-        <div className="flex flex-grow flex-col gap-4 overflow-y-auto  bg-white p-8 dark:bg-slate-800 sm:p-6">
-          <h1 className="text-3xl">{greeter()}</h1>
-          <TodaysScores />
-          <Trends />
-        </div>
-      ) : (
-        <div className=" flex h-[90vh] flex-col items-center justify-center">
-          <h1 className="mb-8 text-xl">
-            Welcome to RingLab. A data dashboard for your Oura Ring.
-          </h1>
-          <SignInButton />
-        </div>
-      )}
-    </>
-  );
+  if (status === "unauthenticated") {
+    useRouter().push("/sign-in");
+  }
+  if (status === "authenticated") {
+    return (
+      <div className="flex flex-grow flex-col gap-4 overflow-y-auto  bg-white p-8 dark:bg-slate-800 sm:p-6">
+        <h1 className="text-3xl">{greeter()}</h1>
+        <TodaysScores />
+        <Trends />
+      </div>
+    );
+  }
 };
 
 export default Home;
---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,28 @@
+import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { SignInButton } from "../components/signInButton";
+import { TodaysScores } from "../components/todaysScores";
+import { Trends } from "../components/charts/overviewTrends";
+import { greeter } from "../lib/greeter";
+export const SignIn: NextPage = () => {
+  const { data: session, status } = useSession();
+  if (status === "authenticated") {
+    return (
+      <p className="flex h-full items-center justify-center dark:bg-slate-700">
+        You are Signed In :)
+      </p>
+    );
+  }
+  return (
+    <>
+      <div className=" flex h-full flex-col items-center justify-center dark:bg-slate-700">
+        <h1 className="mb-8 text-xl">
+          Welcome to RingLab. A data dashboard for your Oura Ring.
+        </h1>
+        <SignInButton />
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
