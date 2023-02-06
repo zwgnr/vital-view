d1fee9c59c56ca7ea2dfb49975b83d2adbfb6226
zwagnr <0x@pop-os.localdomain>
2023-02-06 09:28:38 -0500

    clean up index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,18 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { Fragment, useState } from "react";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { getToken } from "next-auth/jwt";
-import { CurrentScoreCard } from "../components/currentScoreCard";
-import { Loader } from "../components/loader";
-import { Icon } from "@iconify/react";
+import { useSession } from "next-auth/react";
 import { SignInButton } from "../components/signInButton";
-import Router from "next/router";
 import { TodaysScores } from "../components/todaysScores";
 import { Trends } from "../components/charts/overviewTrends";
 
-export async function getServerSideProps({ req }) {
-  const token = await getToken({ req });
-  const authToken = token?.accessToken;
-
-  //console.log("GSSP", token);
-  const getTime = new Date();
-  const currentTime = getTime.toLocaleString().slice(0, 9);
-  return { props: { score: "6", email: "2", age: 21, time: currentTime } };
-}
-
-
-export const Home: NextPage = ({ score, email, age, time }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+export const Home: NextPage = () => {
   const { data: session, status } = useSession();
 
   return (
     <>
       {status === "authenticated" ? (
         <div className="flex flex-grow flex-col gap-4 overflow-y-auto  bg-white p-8 dark:bg-slate-800 sm:p-6">
-          {/* main body */}
           <h1 className="text-3xl">Good Afternoon,</h1>
-            <TodaysScores />
+          <TodaysScores />
           <Trends />
         </div>
       ) : (
