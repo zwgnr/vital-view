d84ad4c126f0952701d3806e38b40589e7da64f8
zwagnr <zachwagner@hey.com>
2023-02-13 12:16:23 -0500

    fix info api Route

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -1,21 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getToken } from "next-auth/jwt";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = await getToken({ req });
-  const token = response?.accessToken;
-  const url = `https://api.ouraring.com/v2/usercollection/personal_info`;
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
+  const session = await getServerSession(req, res, authOptions);
 
-  const ouraResponse = await fetch(url, { headers });
-  const result = await ouraResponse.json();
-  const personalInfo = result;
+  if (!session) {
+    return res
+      .status(401)
+      .json({ message: "You must be logged in to access this content" });
+  } else {
+    const token = session?.accessToken;
+    const url = `https://api.ouraring.com/v2/usercollection/personal_info`;
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
 
-  //console.log("getSleepAuthToken", token);
-  //console.log("getInfo", personalInfo);
-  return res
-    .status(200)
-    .json({ personalInfo: result, email: personalInfo.email });
+    const ouraResponse = await fetch(url, { headers });
+    const result = await ouraResponse.json();
+    const personalInfo = result;
+
+    return res
+      .status(200)
+      .json({ personalInfo: result, email: personalInfo.email });
+  }
 };
