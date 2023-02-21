d9a7497837023013e230b1d649f8200c3297c7b4
zwagnr <zachwagner@hey.com>
2023-02-21 17:48:48 -0500

    readme updates & sign in page font size fix

--- README.md
+++ README.md
@@ -2,14 +2,22 @@
 <img src='./public/ringLabLogo.png' width="150" height="150"/> 
 <h1>RingLab</h1>
 </div>
-
 <h3 align="center">An Analytics Dashboard for your 
   <span>
     <a href='https://ouraring.com/'>Oura Ring.</a>
   </span>
 </h3>
 
+<p align="center">
+<img src='public/sleepPage30DaysLightMode.png' width="100%" height="100%"/> 
+</p>
+
+
 # Usage
+
+coming soon!
+
+# Features
 
 coming soon!
 
--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -23,14 +23,14 @@ export const SignIn: NextPage = () => {
     <>
       <div className="flex h-screen  flex-col items-center justify-center bg-gradient-to-r from-indigo-100 via-indigo-300 to-indigo-600 dark:bg-slate-700">
         <Image
-          src="/ring.png"
+          src="/ringLabLogo.png"
           alt="Oura Ring"
-          width={250}
-          height={250}
+          width={150}
+          height={150}
           className="mb-4"
         />
         <h1 className="mb-8 flex flex-col items-center justify-center gap-2">
-          <p className="text-5xl">Welcome to RingLab.</p>
+          <p className="text-4xl md:text-5xl">Welcome to RingLab.</p>
           <p className="text-xl">A data dashboard for your Oura Ring.</p>
         </h1>
         <SignInButton />
