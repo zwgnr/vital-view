cdd2d3b70721e333781dc3fe8343379931d0a628
zwagnr <zachwagner@hey.com>
2023-02-22 14:40:45 -0500

    add vercel url

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 const redirectURI =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api/auth/callback/oura"
-    : "vercel";
+    : "ring-lab.vercel.app";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
