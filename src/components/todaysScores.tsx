aa96b69c69680744268b648216e24712e27ebc5d
zwagnr <zachwagner@hey.com>
2023-02-22 10:26:14 -0500

    padding fix

--- src/components/todaysScores.tsx
+++ src/components/todaysScores.tsx
@@ -14,13 +14,13 @@ export const TodaysScores = () => {
   const todaysActivity = activity?.rangeDataPoints.score;
 
   return (
-    <div className="h-2/3 w-full rounded-2xl bg-white dark:bg-slate-800  sm:h-1/3">
+    <div className="h-2/3 w-full rounded-2xl bg-white pb-6  dark:bg-slate-800 sm:h-1/3">
       <h1 className="text-md ml-4 mt-4 font-bold">Todays Scores</h1>
       <div className=" flex h-[90%] min-h-0 w-full flex-col rounded-2xl sm:flex-row">
         <div className="flex h-full w-full justify-center overflow-hidden rounded-2xl md:w-1/3 ">
           <div className="flex h-full min-h-0 w-full flex-col items-center justify-center rounded-2xl ">
             {dailySleepLoading ? (
-              <Loader size='h-12 w-12'/>
+              <Loader size="h-12 w-12" />
             ) : (
               <RadialScoreChart label={["Sleep"]} data={todaysSleep} />
             )}
@@ -29,7 +29,7 @@ export const TodaysScores = () => {
         <div className="flex h-full w-full justify-center overflow-hidden  rounded-2xl md:w-1/3 ">
           <div className="flex h-full min-h-0 w-full flex-col items-center justify-center rounded-2xl ">
             {readinessLoading ? (
-              <Loader size='h-12 w-12'/>
+              <Loader size="h-12 w-12" />
             ) : (
               <RadialScoreChart label={["Readiness"]} data={todaysReadiness} />
             )}
@@ -38,7 +38,7 @@ export const TodaysScores = () => {
         <div className="flex h-full w-full justify-center overflow-hidden  rounded-2xl md:w-1/3 ">
           <div className="flex h-full min-h-0 w-full flex-col items-center justify-center rounded-2xl ">
             {activityLoading ? (
-              <Loader size='h-12 w-12'/>
+              <Loader size="h-12 w-12" />
             ) : (
               <RadialScoreChart label={["Activity"]} data={todaysActivity} />
             )}
